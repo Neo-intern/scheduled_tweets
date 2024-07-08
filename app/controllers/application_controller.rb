a715743d747c401affae84e6255bd3019f9e486e
neo-intern <neo.intern@bitcube.tech>
2024-07-08 16:57:46 +0200

    WIP INITIAL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  private
+
+  def current_user
+    Current.user ||= authenticate_user_from_session
+  end
+
+  def authenticate_user_from_session
+    if session[:user_id] && user = User.find_by(id: session[:user_id])
+
+  def login(user)
+    Current.user = user
+    reset_session
+    session[:user_id] = user.id
+  end
+
+  def logout(user)
+    Current.user = nil
+    reset_session
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
+  
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
+
+  resource :session
+  resource :registration
+  resource :password
+  resource :password_reset
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
