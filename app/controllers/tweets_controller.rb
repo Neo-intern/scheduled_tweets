98334da1146a996152f3c9ce78d12354e32bdac7
Neo-intern <neo.intern@bitcube.tech>
2024-07-17 09:37:07 +0200

    WIP Layout complete, Twitter publishing to be implemented

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_tweet, only: [ :edit, :update, :destroy ]
 
   def index
     @tweets = Current.user.tweets
@@ -12,15 +13,35 @@ class TweetsController < ApplicationController
   def create
     @tweet = Current.user.tweets.create(tweet_params)
     if @tweet.save
-      redirect_to @tweet, notice: "Tweet was scheduled successfully"
+      redirect_to tweets_path, notice: "Tweet was scheduled successfully"
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was updated Sucessfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was unscheduled"
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
